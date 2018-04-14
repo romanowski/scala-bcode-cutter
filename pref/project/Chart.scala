449abbde81eaedfdfe7fd64720fabf1c071f132e
Krzysztof Romanowski <romanowski.kr@gmail.com>
2018-04-14 19:46:59 +0200

    WIP_5

--- pref/project/Chart.scala
+++ pref/project/Chart.scala
@@ -7,49 +7,49 @@ import spray.json.DefaultJsonProtocol._
 import spray.json._
 
 object Charts {
-  val warmupChart = TaskKey[Path]("warmupChart")
-  val runBenchmark = TaskKey[Path]("Run bechmark")
+  val warmupChart = TaskKey[Unit]("drawCharts")
+  val runBenchmark = TaskKey[Unit]("runBechmarks")
 
   case class Percentiles(`50.0`: Double, `90.0`: Double, `95.0`: Double, `99.0`: Double)
 
   case class MetricData(score: Double, rawData: Option[Array[Array[Double]]], scorePercentiles: Percentiles)
 
-  case class Benchmark(benchmark: String, primaryMetric: MetricData)
+  case class Benchmark(benchmark: String, primaryMetric: MetricData){
+    def name = benchmark.split('.').last
+  }
 
   private implicit val percentilesFormat = jsonFormat4(Percentiles)
   private implicit val mdFormat = jsonFormat3(MetricData)
   private implicit val benchFormat = jsonFormat2(Benchmark)
 
+  def chartBase = file("charts").getAbsoluteFile
+  def jsonLocation = file("out.json").toPath.toAbsolutePath
+  def txtLocation = file("out.txt").toPath.toAbsolutePath
 
   private def drawWarmingCharts(data: Array[Benchmark], base: File): Unit = {
-    val batch = 10
-    val initial = 300
-    val margin = 4
-
-
     (chartBase / "warming-full").mkdir()
     (chartBase / "warming-partial").mkdir()
 
-    data.zipWithIndex.foreach { case (metric, index) =>
-      val rawTimes = metric.primaryMetric.rawData.get.head
-      val avg = metric.primaryMetric.scorePercentiles.`50.0`
+    data.zipWithIndex.foreach { case (benchmark, index) =>
+      val rawTimes = benchmark.primaryMetric.rawData.get.head
+      val avg = benchmark.primaryMetric.scorePercentiles.`50.0`
       val strAvg = "%.4f".format(avg)
 
       def draw(rawData: Array[Double]): Unit = {
-        val name = metric.benchmark.split('.').last
+        import benchmark._
         val csv = rawData.zipWithIndex.map { case (t, i) => s"$i\t$t" }.mkString("\n")
         val dataFile = base / s"data-$name.csv"
         IO.write(dataFile, csv)
         val file = (base / s"plot-$name.sh").getAbsoluteFile
         val fullChart = rawData == rawTimes
+        val ls = index % 9
         val command =
-          s"""set term png size 1920, 1080
+          s"""set term png size ${if (fullChart) 1920 else 1920 / 2}, 1080
              |set output '$chartBase/warming-${if(fullChart) "full" else "partial"}/${name.replace(" ", "-").toLowerCase}.png'
              |set title '$name'
              |${if(fullChart) "" else "set logscale y 2"}
              |set yrange [1:${if (fullChart) 28 else 256}]
-             |# set arrow from $initial, graph 0 to $initial, graph 4 nohead
-             |plot '$dataFile' with lines lt $index title 'times', $avg title 'p0.50 = $strAvg' lt ${index + 1} lw 3
+             |plot '$dataFile' with lines lt $ls title 'times', $avg title 'p0.50 = $strAvg' lt ${ls + 1} lw 3
              |""".stripMargin
 
         IO.write(file, command)
@@ -62,34 +62,42 @@ object Charts {
     }
   }
 
-  def chartBase = file("charts").getAbsoluteFile
-
-  private def summary(data: Array[Benchmark], base: File, chartName: String): Unit = {
+  private def summary(baseData: Array[Benchmark], base: File, chartName: String, charKind: String): Unit = {
     val filename = chartName.replace(" ", "-")
-    val csv = data.sortBy(_.primaryMetric.scorePercentiles.`50.0`).map { b =>
+    def values(b: Benchmark) = {
       val p = b.primaryMetric.scorePercentiles
-      Seq(b.benchmark.split('.').last, p.`99.0`, p.`95.0`, p.`90.0`, p.`50.0`).mkString("\t")
-    }.mkString("\n")
+      Seq(b.name, p.`99.0`, p.`95.0`, p.`90.0`, p.`50.0`)
+    }
+
+    val data = baseData.toList
+    val baseline = data.filter(_.name  == "baseline")
+
+    val csv = data.sortBy(_.primaryMetric.scorePercentiles.`90.0`).map(values(_).mkString("\t")).mkString("\n")
     val dataFile = base / s"$filename.csv"
     IO.write(dataFile, csv)
 
     def plotSet(name: String, column: Int) =
       s"'$dataFile' using ${column + 2}:xtic(1) title '$name'"
 
-    val plotCmd = Seq("99.0", "95.0", "90.0", "50.0").zipWithIndex.map((plotSet _).tupled)
+    val names = Seq("p99.0", "p95.0", "p90.0", "p50.0")
+
+    val baselineCmd = baseline.map(_.primaryMetric.scorePercentiles.`95.0`)
+      .map(v => s"$v ls 6 lw 3 title 'baseline'")
+
+    val plotCmd = names.zipWithIndex.map((plotSet _).tupled) ++ baselineCmd
 
     val command =
       s"""
-         |set term png size 1920, 1080
-         |set output '$chartBase/$filename.png'
+         |set term png size 1920, 1080 font "Helvetica,22"
+         |set output '$chartBase/$filename-$charKind.png'
          |set key left
          |set grid y
-         |set xtics rotate
+         |${if(data.size < 6) "#" else ""}set xtics rotate
          |set style data histograms
          |#set style histogram rowstacked
          |set boxwidth 0.80
          |set style fill solid 1.0 border -1
-         |set title '$chartName'
+         |set title '$chartName [us/op] - $charKind'
          |plot ${plotCmd.mkString(", ")}
       """.stripMargin
 
@@ -99,32 +107,35 @@ object Charts {
     s"gnuplot $scriptFile".!
   }
 
+  val charts = Map[String, String => Boolean](
+    "All" -> (_ => true),
+    "Baselines" -> (_.contains("baseline")),
+    "Java vs. Scalac" -> Set("baseline", "javaOOO", "cake"),
+    "Java, OOO in Scala and Cake" -> Set("baseline", "javaOOO", "cake", "oooTree")
+  )
+
   def implementWarmupChart = warmupChart := {
     val base = streams.value.cacheDirectory
-    val json = IO.read(runBenchmark.value.toFile).parseJson
-    val data = json.convertTo[Array[Benchmark]]
-    streams.value.log.success(s"Saving to $base using: ${data.toSeq.mkString("\n")}")
+    val jsonData = jsonLocation.toFile
+    assert(jsonData.exists(), "Run benchmarks before!")
+    val data = IO.read(jsonData).parseJson.convertTo[Array[Benchmark]]
+    //streams.value.log.success(s"Saving to $base using: ${data.toSeq.mkString("\n")}")
     val (warming, hot) = data.partition(_.primaryMetric.rawData.isDefined)
 
     chartBase.mkdir()
+
     drawWarmingCharts(warming, base)
-    summary(hot, base, "Hot benchmarks")
-    summary(warming, base, "Warming benchmarks")
-    base.toPath
+
+    for ((title, isApplicable) <- charts) {
+      summary(hot.filter(b => isApplicable(b.name)), base, title, "Hot")
+      summary(warming.filter(b => isApplicable(b.name)), base, title, "Warming")
+    }
   }
 
-
-  def jsonLocation = file("out.json").toPath.toAbsolutePath
-  def txtLocation = file("out.txt").toPath.toAbsolutePath
-
+  private val benchmarkCmd = s" .*Benchmark.* -rff $jsonLocation -rf json -o $txtLocation"
 
   def settings = Seq(
-    runBenchmark := {
-      val keepThis = JmhKeys.Jmh
-      run.in(JmhKeys.Jmh).toTask(
-        s" .*Benchmark.* -rff $jsonLocation -rf json -o $txtLocation").value
-      jsonLocation
-    },
+    runBenchmark := run.in(JmhKeys.Jmh).toTask(benchmarkCmd).value,
     implementWarmupChart
   )
 
--- pref/src/main/scala/rpg/SkillTrees.scala
+++ pref/src/main/scala/rpg/SkillTrees.scala
@@ -3,33 +3,26 @@ package rpg
 import org.openjdk.jmh.annotations.CompilerControl
 import org.openjdk.jmh.annotations.CompilerControl.Mode.DONT_INLINE
 
-class PlayerTraits(strCost_ : Int,
-                   dexCost_ : Int,
-                   wisCost_ : Int) {
-  @CompilerControl(DONT_INLINE) def strCost: Int = strCost_
-
-  @CompilerControl(DONT_INLINE) def dexCost: Int = dexCost_
-
-  @CompilerControl(DONT_INLINE) def wisCost: Int = wisCost_
+class PlayerTraits(str : Int, dex : Int, wis : Int) {
+  @CompilerControl(DONT_INLINE) def strCost: Int = str
+  @CompilerControl(DONT_INLINE) def dexCost: Int = dex
+  @CompilerControl(DONT_INLINE) def wisCost: Int = wis
 }
 
 abstract class SkillTreeRepr[T](val tree: T) {
-  def totalCost(): Int
+  @CompilerControl(DONT_INLINE) def totalCost(): Int
 }
 
 abstract class SkillTrees {
-  /**
-    * Dex --> Dex -> Wis --> Wis("aimed")
-    * \                   \-> Wis -> Dex("rapid")
-    * \-> Str --> Str("power")
-    * \-> Str -> Dex("double")
-    */
+  /** Dex --> Dex -> Wis --> Wis("aimed")
+    *      \              \-> Wis -> Dex("rapid")
+    *       \-> Str --> Str("power")
+    *               \-> Str -> Dex("double") */
   def archeryTree(playerTraits: PlayerTraits): SkillTreeRepr[_]
 
   /** Wis -> Wis --> Wis("1")
     *            \-> Dex -> Dex --> Dex("2")
     *                           \-> Str -> Str --> Str("3")
-    *                                          \-> Wis("4")
-    */
+    *                                          \-> Wis("4") */
   def charismaTree(traits: PlayerTraits): SkillTreeRepr[_]
 }
